2af0c94f46c65ed998c24421aeef64b7a1f451ee
Jefferson <88409973+BugaroxD@users.noreply.github.com>
2024-02-29 13:33:41 -0300

    Delete server directory

--- server/src/routes/example.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { Example } from "../service/example";
-
-const router = Router();
-const example = new Example();
-
-router.get("/", example.getExampleJson);
-
-export default router;
