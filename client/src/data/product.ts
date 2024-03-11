fcbc3be973610f3567f0bd7541e295c58621e312
vitorsouzaJ <63322105+vitorsouzaJ@users.noreply.github.com>
2024-03-11 20:21:34 -0300

    Feat:Criação do Json com dados dos produtos

---
+++ client/src/data/product.ts
@@ -0,0 +1,155 @@
+import { IProducts } from "../types/cart";
+
+// eslint-disable-next-line no-undef
+export const product: IProducts[] = [
+  {
+    id: "d811566a-07b5-4340-bdbd-5125fd5207b2",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Forceland Vitality F22",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r16-91v-forceland-vitality-f22-1.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300",
+  },
+  {
+    id: "a4660305-6f7c-4c44-8d27-6ea6fd4cfd23",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Barum Bravuris 5HM 91V",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r16-barum-bravuris-5hm-91v-1.png?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300&format=jpeg",
+  },
+  {
+    id: "471c0434-4700-4ff8-ae5b-832509763c60",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "91H Hankook Kinergy Eco K425",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r16-91h-hankook-kinergy-eco-k425-1.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300",
+  },
+  {
+    id: "a4660305-6f7c-4c44-8d27-6ea6fd4cfd23",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Goodyear Kelly Edge Sport 2 91V",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r16-goodyear-kelly-edge-sport-2-91v-1.png?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300&format=jpeg",
+  },
+  {
+    id: "d811566a-07b5-4340-bdbd-5125fd5207b2",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Pirelli Cinturato P7 KS 91V",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r16-pirelli-cinturato-p7-ks-91v-1.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300",
+  },
+  {
+    id: "92ebc3a9-9146-43b5-8b6b-8b6019027609",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Bridgestone Ecopia EP150 91V",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r16-bridgestone-ecopia-ep150-91v-1.png?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300&format=jpeg",
+  },
+  {
+    id: "12255485-3f61-4cef-83c7-b6b30e5de186",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Goodyear Wrangler Territory AT 91H",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r16-goodyear-wrangler-territory-at-91h-1.png?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300&format=jpeg",
+  },
+  {
+    id: "1ba0ab2b-42d1-45b1-9f71-de0baf27cb67",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Continental RUN FLAT ContiPremiu...",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r16-continental-run-flat-contipremiumcontact-ssr-91w-1.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300",
+  },
+  {
+    id: "28699ac3-ba64-4c6a-b2db-41b171fa8843",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Continental PremiumContact 6 91V",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r16-continental-premiumcontact-6-91v-1.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300",
+  },
+  {
+    id: "30939aa4-cbee-4717-8b65-3ed11f4d838d",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Bridgestone RUN FLAT Turanza ER3...",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r16-bridgestone-run-flat-turanza-er300-91w-1.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300",
+  },
+  {
+    id: "f44c5fdb-757e-4038-b3d7-e1928cea0117",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "95W Forceland Vitality F22",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r17-95w-forceland-vitality-f22-1.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300",
+  },
+  {
+    id: "de5ca21f-e6ba-4e08-ab7e-427f4b4a0bbf",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Hankook Kinergy GT H436 91H",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r17-hankook-kinergy-gt-h436-91h-1.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300",
+  },
+  {
+    id: "5caa9734-14c0-43ab-a2eb-7b931b255eb8",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Goodyear Wrangler Territory HT 91V",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r17-goodyear-wrangler-territory-ht-91v-1.png?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300&format=jpegg",
+  },
+  {
+    id: "f9e86d2a-1f7c-4262-ad61-cfb16aa9d4c2",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Bridgestone Turanza T005 91V",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/-/1/-1_89.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300",
+  },
+  {
+    id: "91691738-5a35-4822-889f-5f81e9878732",
+    price: 274.9,
+    width: 205,
+    height: 55,
+    rimSizes: [{ rim: 16 }, { rim: 17 }, { rim: 18 }],
+    name: "Continental ContiPowerContact 91...",
+    imageUrl:
+      "https://www.acheipneus.com.br/media/catalog/product/p/n/pneu-20555r17-continental-contipowercontact-91v-original-nissan-kicks-fluence-1.jpg?optimize=high&bg-color=255,255,255&fit=bounds&height=300&width=240&canvas=240:300",
+  },
+];
--- client/src/types/cart.ts
+++ client/src/types/cart.ts
@@ -9,3 +9,13 @@ interface IProduct {
 export interface ICartState {
   items: IProduct[];
 }
+
+export interface IProducts {
+  id: string;
+  price: number;
+  name: string;
+  imageUrl: string;
+  width: number;
+  height: number;
+  rimSizes: { rim: number }[];
+}
